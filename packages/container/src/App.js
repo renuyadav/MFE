e00646ef8c5dd254c93e631a8b1796ae7a80bba4
Renu Yadav <renu.yadav@publicissapient.com>
2023-05-10 04:50:04 +0530

    changed working directory

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default(() => {
    return ( 
     <div>
-        <h1>Hi, I am Container of MFE</h1>
+        <h1>Hi, I am Container of MFE packages</h1>
         <hr/>
         <MarketingApp/>
     </div>
