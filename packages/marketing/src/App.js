67e9faa75310c53f8209f4bff391df9f3a3c0f12
Renu Yadav <renu.yadav@publicissapient.com>
2023-05-09 14:26:11 +0530

    test marketing change

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -8,7 +8,7 @@ import Pricing from './components/Pricing';
 export default () => {
   return (
     <div>
-      <div>I am MarketingApp</div>
+      <div>Marketing App</div>
       <StylesProvider>
         <BrowserRouter>
           <Switch>
