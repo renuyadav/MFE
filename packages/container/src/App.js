b42cd95b3c7eb8809e1fe6afa9743d618ba02c80
Renu Yadav <renu.yadav@publicissapient.com>
2023-05-09 12:07:41 +0530

    changed in container code

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default(() => {
    return ( 
     <div>
-        <h1>Hi Container</h1>
+        <h1>Hi, I am the Container of MFE packages</h1>
         <hr/>
         <MarketingApp/>
     </div>
