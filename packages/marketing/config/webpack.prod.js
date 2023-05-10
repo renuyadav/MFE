b0d3c3973abb7c1470e959d1812cdcf50f442fc9
Renu Yadav <renu.yadav@publicissapient.com>
2023-05-10 12:51:18 +0530

    new workflow

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename:'[name].[contenthash].js',
-        publicPath: '/'
+        publicPath: '/marketing'
     },
     plugins:[
        new ModuleFederationPlugin({
