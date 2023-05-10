c1c4efd83b850dfc56009ffd55093789e63f4f39
Renu Yadav <renu.yadav@publicissapient.com>
2023-05-10 11:36:09 +0530

    change public path

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename:'[name].[contenthash].js',
-        publicPath: '/marketing/dist'
+        publicPath: '/'
     },
     plugins:[
        new ModuleFederationPlugin({
