b10da282e9d6e84444d9eeca656b39032611e038
Renu Yadav <renu.yadav@publicissapient.com>
2023-05-12 13:22:33 +0530

    latest deploy 1

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,7 +3,7 @@ const commonConfig = require('./webpack.common')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
-const domain = process.env.PROD_DOMAIN;
+const domain = process.env.MARKETING_SITE_NAME;
 
 
 const prodConfig = {
