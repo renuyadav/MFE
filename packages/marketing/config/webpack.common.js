958205d05c131cf89d4afd14dc2792424e505003
Renu Yadav <renu.yadav@publicissapient.com>
2023-05-15 13:58:58 +0530

    deploy11

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -16,5 +16,10 @@ module.exports = {
         },
       ]
     },
+    plugins:[
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      }),
+    ]
   };
   
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -13,9 +13,6 @@ const devConfig = {
         },
     },
     plugins:[
-        new HtmlWebpackPlugin({
-          template: './public/index.html'
-        }),
        new ModuleFederationPlugin({
         name: 'marketing',
         filename: 'remoteEntry.js',
