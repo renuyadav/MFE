58f068ff7f46f73e9d119803e3e47da1339bac3d
Renu Yadav <renu.yadav@publicissapient.com>
2023-06-23 11:35:44 +0530

    fixed path in dashboard

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -2,6 +2,11 @@ const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
+const path = require('path');
+
+const domain = process.env.PROD_DOMAIN;
+
+console.log("domain name in dashboard config>>", domain);
 
 const prodConfig = {
   mode: 'production',
